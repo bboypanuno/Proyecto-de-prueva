72de131cc3a50b3ca26091317eba7f7dad47f1c8
Andres Castillo <acastillo241290@gmail.com>
2016-04-21 12:49:29 +0200

    Segunda version

--- NuevoProyecto.java
+++ NuevoProyecto.java
@@ -1,5 +1,8 @@
 public class NuevoProyecto{
  public static  void main(String [] args){
-   System.out.println("holamundo");
+   System.out.println("hola mundo segunda version");
+   
+   int array[] = new int[5];
+   System.out.println(array[2]);
  }
 }
