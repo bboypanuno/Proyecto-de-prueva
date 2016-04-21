5f1cdd6b0d2db71a4de68cd17f60cbc482bddfac
Andres Castillo <acastillo241290@gmail.com>
2016-04-21 12:52:03 +0200

    Tercera version

--- NuevoProyecto.java
+++ NuevoProyecto.java
@@ -1,5 +1,8 @@
 public class NuevoProyecto{
  public static  void main(String [] args){
    System.out.println("holamundo");
+   
+   int array[]= new int[20];
+   System.out.println(array[20]);
  }
 }
